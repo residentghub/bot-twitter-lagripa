5e926b0b1bcf9ea9aec4c792d66b0fab08935861
Novoa <emerson.novoa@MEXONL-NMX0022.local>
2018-07-20 10:53:27 -0500

    Ready to deploy on Heroku

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     //Twitter account La Gripa Test
-    consumer_key:         'uF2yr7tH9dpMROC9cGf0r5YdK',
-    consumer_secret:      'MidQXyo5Op81K7l9KqOYf5t5MF65kx0y6BiKs002I9x4h1hyjY',
-    access_token:         '1014942198201290754-fOxWk0sCnsPGT6006vv43xd9RMPwmD',
-    access_token_secret:  'yyPfM8T2QWq4WwwULoeqp1EbstylyyBC2Ua9A9MwpCWXi',
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
 }
