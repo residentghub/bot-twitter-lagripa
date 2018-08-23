f303aeea754c97a5d192d52a67f7d7a4429f1030
Novoa <emerson.novoa@MEXONL-NMX0022.local>
2018-08-23 16:38:24 -0500

    Start up

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     //Twitter account La Gripa Test
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  '',
+    consumer_key:         'd1YGCWEd2vz1LiMCJtkQrkp0A',
+    consumer_secret:      'hoiUglQvtc32nvTgRpfeARkiqeuN5YOsDZIiiGdBrrT6Hyt59r',
+    access_token:         '904574215260602369-odTBoN6IdITZUEP52AbI2QlvKcHZe7b',
+    access_token_secret:  'MKe7a0aIzz4sfodVbC8omu1fskNdUB4ZzXIRmSlPBHObs',
 }
